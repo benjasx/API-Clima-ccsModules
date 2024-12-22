5646abdf07f34cb19ef07f398cce6f81a6290396
Benjasx <benjasanchez175@gmail.com>
2024-12-22 01:34:22 -0700

    Agregando estilos al formulario

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,14 +1,15 @@
 import { countries } from "../../data/countries";
+import styles from './Form.module.css'
 
 export default function Form() {
   return (
-    <form>
-      <div>
+    <form className={styles.form}>
+      <div className={styles.field}>
         <label htmlFor="city">Ciudad:</label>
         <input id="city" type="text" name="city" placeholder="Ciudad" />
       </div>
 
-      <div>
+      <div className={styles.field}>
         <label htmlFor="pais">Pais:</label>
         <select>
             <option value="">--Seleccione un país-- </option>
@@ -18,7 +19,7 @@ export default function Form() {
         </select>
 
       </div>
-        <input type="submit" value="Consultar Clima" />
+        <input className={styles.submit} type="submit" value="Consultar Clima" />
     </form>
   );
 }
