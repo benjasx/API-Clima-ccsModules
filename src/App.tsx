432d4898a893d0803cb3d3eff7f56fc5eac376a6
Benjasx <benjasanchez175@gmail.com>
2024-12-22 00:49:37 -0700

    Creando proyecto de clima con API

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import styles from "./app.module.css"
+
+function App() {
+  return (
+    <>
+      <h1 className={styles.title}>Buscador de clima</h1>
+
+      <div className={styles.container}>
+        <h2>1</h2>
+        <h2>2</h2>
+      </div>
+    </>
+  )
+}
+export default App
