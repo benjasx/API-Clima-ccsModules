18a6342b091470f2fb76ea1e98b03d7a9872449c
Benjasx <benjasanchez175@gmail.com>
2024-12-22 01:46:52 -0700

    colocando los datos en el state del clima a consultar

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,25 +1,47 @@
+import { useState } from "react";
 import { countries } from "../../data/countries";
-import styles from './Form.module.css'
+import styles from "./Form.module.css";
+import { SearchType } from "../../types";
 
 export default function Form() {
+  const [search, setSearch] = useState<SearchType>({
+    city: "",
+    country: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement> ) =>{
+    setSearch({
+        ...search,
+        [e.target.name]: e.target.value
+    })
+  }
+
   return (
     <form className={styles.form}>
       <div className={styles.field}>
         <label htmlFor="city">Ciudad:</label>
-        <input id="city" type="text" name="city" placeholder="Ciudad" />
+        <input
+          id="city"
+          type="text"
+          name="city"
+          placeholder="Ciudad"
+          value={search.city}
+          onChange={ handleChange}
+        />
       </div>
 
       <div className={styles.field}>
         <label htmlFor="pais">Pais:</label>
-        <select>
-            <option value="">--Seleccione un país-- </option>
-            {countries.map(country => (
-                <option key={country.code} value={country.code}>{country.name}</option>
-            ))}
+        <select id="country" name="country" value={search.country} onChange={handleChange}>
+          <option value="">--Seleccione un país-- </option>
+          {countries.map((country) => (
+            <option key={country.code} value={country.code}>
+              {country.name}
+            </option>
+          ))}
         </select>
-
       </div>
-        <input className={styles.submit} type="submit" value="Consultar Clima" />
+      <input className={styles.submit} type="submit" value="Consultar Clima" />
     </form>
   );
 }
